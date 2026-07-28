8429c77acadcf665b953b727c2483fb2604de9f2
viniciuspascucci1993 <vinicius.pascucci1@gmail.com>
2026-07-28 11:19:13 -0300

    feat: initial project setup

---
+++ src/main/java/com/ironvault/merchants/IronvaultMerchantsApplication.java
@@ -0,0 +1,13 @@
+package com.ironvault.merchants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronvaultMerchantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IronvaultMerchantsApplication.class, args);
+	}
+
+}
